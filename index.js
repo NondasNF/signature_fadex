012b31a9ed711f5b9f40ac6dc4066bee034f2e5b
Epaminondas Noronha Feitosa <nondasnoronha@gmail.com>
2021-09-07 22:33:00 -0300

    Update of code html and readme

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ function set_user_information(){
     let nameNewAdd = document.querySelector("#name_box").value;
     let jobnameNewAdd = document.querySelector("#jobname_box").value;
     let emailNewAdd = document.querySelector("#email_box").value;
-    
-    // let emailTo = document.querySelector("#person_email").href;
-    // let nameOld = document.getElementById("person_name").innerHTML;
-    // let jobnameOld = document.getElementById("person_jobname").innerHTML;
-    // let emailOld = document.getElementById("person_email").innerHTML;
 
     document.getElementById("person_name").innerHTML = `${nameNewAdd}<span><br></span>`;
     document.getElementById("person_jobname").innerHTML = jobnameNewAdd;
