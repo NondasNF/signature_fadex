a359d1ca744d2b8f1991601c05de6228c7c5ff39
Epaminondas Noronha Feitosa <nondasnoronha@gmail.com>
2021-09-09 15:22:51 -0300

    Added a new desing and responsivity

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ function set_user_information(){
     let nameNewAdd = document.querySelector("#name_box").value;
     let jobnameNewAdd = document.querySelector("#jobname_box").value;
     let emailNewAdd = document.querySelector("#email_box").value;
-
+if(nameNewAdd){
     document.getElementById("person_name").innerHTML = `${nameNewAdd}<span><br></span>`;
     document.getElementById("person_jobname").innerHTML = jobnameNewAdd;
     document.getElementById("person_email").innerHTML = emailNewAdd;
     document.querySelector("#person_email").href = `mailto:${emailNewAdd}`;
+  }else{
+    alert("Por favor, preencha todos os campos");
+  }
+}
+function delete_user_information(){
+    document.querySelector("#name_box").value = "";
+    document.querySelector("#jobname_box").value = "";
+    document.querySelector("#email_box").value = "";
 }
 
 var Tabela = {
