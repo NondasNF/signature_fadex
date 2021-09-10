f651e0c93e401769e1ed70af359da0a030d785fd
Epaminondas Noronha Feitosa <nondasnoronha@gmail.com>
2021-09-10 11:46:38 -0300

    Js alert and scale fix in CSS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function set_user_information(){
     let nameNewAdd = document.querySelector("#name_box").value;
     let jobnameNewAdd = document.querySelector("#jobname_box").value;
     let emailNewAdd = document.querySelector("#email_box").value;
-if(nameNewAdd){
+if(nameNewAdd !="" && jobnameNewAdd !="" && emailNewAdd !=""){
     document.getElementById("person_name").innerHTML = `${nameNewAdd}<span><br></span>`;
     document.getElementById("person_jobname").innerHTML = jobnameNewAdd;
     document.getElementById("person_email").innerHTML = emailNewAdd;
